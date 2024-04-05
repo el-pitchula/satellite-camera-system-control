cd4aef7233697fa1947149216bf8b17400c66f04
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2024-04-05 13:49:52 -0300

    main

---
+++ readme.md
@@ -0,0 +1,70 @@
+<h1 align="center">Controle PID do sistema</h1>
+<h1 align="center">câmera-satélite com arduino</h1>
+
+
+<div align="center">
+  <strong>🚀 add descrição </strong>
+</div>
+
+<div align="center">
+  <p>palavras-chave</p>
+  <p></p>
+</div>
+
+## 📖 Índice
+
+- [Visão Geral](#visão-geral)
+- [Tecnologias](#tecnologias)
+- [Configuração do Ambiente](#configuração-do-ambiente)
+- [Como Contribuir](#como-contribuir)
+
+## 🔭 Visão Geral
+
+falar sobre a comunicação entre os codigos e link do artigo
+
+## 💻 Tecnologias (add icons)
+
+- C/C++ -> arduino (.ino)
+- Python (.py)
+- ...
+
+add links para as respectivas documentações ou sites oficiais
+
+## ⚙️ Configuração do Ambiente
+
+fornecer instruções claras e detalhadas sobre como configurar o ambiente de desenvolvimento localmente:
+
+- Pré-requisitos
+- Instalação de dependências
+- Configuração do banco de dados
+- Configuração de variáveis de ambiente
+- Execução de migrações ou scripts de inicialização
+- ...
+
+fornecer exemplos de comandos ou scripts necessários para executar o projeto corretamente
+
+## Como Contribuir
+
+Se você deseja contribuir para o projeto, siga estas etapas:
+
+1. Faça um fork do repositório e clone-o em sua máquina local.
+2. Crie uma nova branch para suas modificações:
+   ```
+   git checkout -b minha-branch
+   ```
+3. Faça as modificações desejadas e adicione-as ao stage:
+   ```
+   git add .
+   ```
+4. Faça um commit das suas alterações:
+   ```
+   git commit -m "Minhas modificações"
+   ```
+5. Envie suas alterações para o repositório remoto:
+   ```
+   git push origin minha-branch
+   ```
+6. Abra um pull request para que suas modificações sejam revisadas e incorporadas ao projeto.
+
+
+Aproveite o projeto!
---
+++ tests/MPU6050.ino
@@ -0,0 +1,38 @@
+include<Wire.h> //Biblioteca para comunicação I2C
+
+const int MPU_addr=0x68; //Endereço do sensor
+
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; //Variaveis para pegar os valores medidos
+
+void setup(){
+  Wire.begin(); //Inicia a comunicação I2C
+  Wire.beginTransmission(MPU_addr); //Começa a transmissao de dados para o sensor
+  Wire.write(0x6B); // registrador PWR_MGMT_1
+  Wire.write(0); // Manda 0 e "acorda" o MPU 6050
+  Wire.endTransmission(true);
+
+  Serial.begin(9600); //Inicia a comunicaçao serial (para exibir os valores lidos)
+}
+void loop(){
+  Wire.beginTransmission(MPU_addr); //Começa a transmissao de dados para o sensor
+  Wire.write(0x3B); // registrador dos dados medidos (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr,14,true); // faz um "pedido" para ler 14 registradores, que serão os registrados com os dados medidos
+  AcX=Wire.read()<<8|Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY=Wire.read()<<8|Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ=Wire.read()<<8|Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp=Wire.read()<<8|Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX=Wire.read()<<8|Wire.read(); // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  GyY=Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  GyZ=Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  //Agora escreve os valores no monitor serial
+  Serial.print("AcX = "); Serial.print(AcX);
+  Serial.print(" | AcY = "); Serial.print(AcY);
+  Serial.print(" | AcZ = "); Serial.print(AcZ);
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53); //Equação da temperatura em Cº de acordo com o datasheet
+  Serial.print(" | GyX = "); Serial.print(GyX);
+  Serial.print(" | GyY = "); Serial.print(GyY);
+  Serial.print(" | GyZ = "); Serial.println(GyZ);
+  delay(333);
+}
