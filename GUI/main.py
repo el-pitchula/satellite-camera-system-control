4d89be383dc579d2879352d2979c6df852b15ae5
Jay Jay <64316338+el-pitchula@users.noreply.github.com>
2024-04-10 14:51:58 -0300

    main

--- GUI/main.py
+++ GUI/main.py
@@ -22,7 +22,6 @@ print('Dados recebidos:', dados)
 ser.close() # fechar a comunicação serial
 """
 
-
 import tkinter as tk
 from PIL import Image, ImageTk
 import cv2
@@ -31,88 +30,76 @@ from mayavi import mlab
 import serial
 import time
 
-# Inicializar a comunicação serial
-ser = serial.Serial('COMX', 9600)  # Substitua 'COMX' pela porta serial correta
+ser = serial.Serial('COMX', 9600)
 
-# Função para enviar comandos para o Arduino
 def enviar_comando(comando):
     ser.write(comando.encode())
 
-# Função para receber dados do Arduino
 def receber_dados():
     return ser.readline().decode().strip()
 
-# Função para capturar e exibir a visão da câmera
 def capturar_camera():
-    cap = cv2.VideoCapture(0)  # Captura de vídeo da câmera (substitua 0 pela câmera correta)
+    cap = cv2.VideoCapture(0)
     
     while True:
-        ret, frame = cap.read()  # Captura um quadro do vídeo
+        ret, frame = cap.read() # em vídeo
         
         if ret:
-            # Converter a imagem OpenCV em formato adequado para Tkinter
+            # converter a imagem OpenCV em formato adequado para Tkinter
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             img = Image.fromarray(img)
             imgtk = ImageTk.PhotoImage(image=img)
             
-            # Exibir a imagem na interface
+            # exibir a imagem na interface
             label_camera.imgtk = imgtk
             label_camera.config(image=imgtk)
         
-        # Atualizar a interface
+        # real time
         janela.update_idletasks()
         janela.update()
 
-# Criar janela principal
+# janela principal
 janela = tk.Tk()
 janela.title('Estabilidade da Câmera no Satélite')
 
-# Criar label para exibir a visão da câmera
+# label para exibir a visão da câmera
 label_camera = tk.Label(janela)
 label_camera.pack()
-
-# Botão para iniciar a captura da câmera
+# botão para iniciar câmera
 botao_capturar = tk.Button(janela, text='Capturar Câmera', command=capturar_camera)
 botao_capturar.pack()
 
-# Função para resetar o sistema
 def resetar_sistema():
-    enviar_comando('R')  # Enviar comando 'R' para resetar o sistema no Arduino
-    time.sleep(1)  # Aguardar 1 segundo
-    dados = receber_dados()  # Receber dados do Arduino
+    enviar_comando('R')  # enviar comando 'R' para resetar o sistema no Arduino
+    time.sleep(1)
+    dados = receber_dados()
     print('Dados recebidos:', dados)
 
-# Criar botão de reset
+# botão reset
 botao_reset = tk.Button(janela, text='Resetar Sistema', command=resetar_sistema)
 botao_reset.pack()
 
-# Função para mostrar o monitor serial em uma nova janela
+# monitor serial em uma nova janela
 def mostrar_monitor_serial():
-    # Lógica para mostrar o monitor serial em uma nova janela
+    # criar lógica para mostrar o monitor serial
     pass
 
-# Carregar o ícone do monitor serial
 icon_serial = Image.open('Python/assets/icon_serial.png')
-icon_serial = icon_serial.resize((32, 32), Image.ANTIALIAS)  # Redimensionar o ícone se necessário
+icon_serial = icon_serial.resize((32, 32), Image.ANTIALIAS) # redimencionar
 icon_serial = ImageTk.PhotoImage(icon_serial)
-
-# Criar botão com o ícone para abrir o monitor serial
 botao_serial = tk.Button(janela, image=icon_serial, command=mostrar_monitor_serial)
 botao_serial.pack()
 
-# Função para mostrar informações relevantes do projeto em uma nova janela
+# info em uma nova janela
 def mostrar_informacoes():
-    # Lógica para mostrar informações relevantes do projeto em uma nova janela
+    # criar lógica para mostrar informações do projeto
     pass
 
-# Carregar o ícone de informações
 icon_info = Image.open('Python/assets/icon_info.png')
-icon_info = icon_info.resize((32, 32), Image.ANTIALIAS)  # Redimensionar o ícone se necessário
+icon_info = icon_info.resize((32, 32), Image.ANTIALIAS)
 icon_info = ImageTk.PhotoImage(icon_info)
-
-# Criar botão com o ícone para mostrar informações relevantes
 botao_info = tk.Button(janela, image=icon_info, command=mostrar_informacoes)
 botao_info.pack()
 
-# Loop principal da interface gráfica
+# loop principal da interface gráfica (GUI)
 janela.mainloop()
--- tests/MPU6050.ino
+++ tests/MPU6050.ino
@@ -1,23 +1,24 @@
-#include <Wire.h> //Biblioteca para comunicação I2C
+#include <Wire.h> //comunicação I2C
 
-const int MPU_addr=0x68; //Endereço do sensor
-
-int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ; //Variaveis para pegar os valores medidos
+const int MPU_addr=0x68;
+int16_t AcX,AcY,AcZ,Tmp,GyX,GyY,GyZ;
 
 void setup(){
-  Wire.begin(); //Inicia a comunicação I2C
-  Wire.beginTransmission(MPU_addr); //Começa a transmissao de dados para o sensor
+  Wire.begin();
+
+  Wire.beginTransmission(MPU_addr);
   Wire.write(0x6B); // registrador PWR_MGMT_1
   Wire.write(0); // Manda 0 e "acorda" o MPU 6050
   Wire.endTransmission(true);
 
-  Serial.begin(9600); //Inicia a comunicaçao serial (para exibir os valores lidos)
+  Serial.begin(9600);
 }
+
 void loop(){
-  Wire.beginTransmission(MPU_addr); //Começa a transmissao de dados para o sensor
+  Wire.beginTransmission(MPU_addr); //transmissao de dados para o sensor
   Wire.write(0x3B); // registrador dos dados medidos (ACCEL_XOUT_H)
   Wire.endTransmission(false);
-  Wire.requestFrom(MPU_addr,14,true); // faz um "pedido" para ler 14 registradores, que serão os registrados com os dados medidos
+  Wire.requestFrom(MPU_addr,14,true); // request para ler 14 registradores (para os dados medidos)
   AcX=Wire.read()<<8|Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
   AcY=Wire.read()<<8|Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
   AcZ=Wire.read()<<8|Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
@@ -26,18 +27,17 @@ void loop(){
   GyY=Wire.read()<<8|Wire.read(); // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read(); // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 
-  //Agora escreve os valores no monitor serial
   Serial.print("AcX = "); Serial.print(AcX);
   Serial.print(" | AcY = "); Serial.print(AcY);
   Serial.print(" | AcZ = "); Serial.print(AcZ);
-  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53); //Equação da temperatura em Cº de acordo com o datasheet
+  Serial.print(" | Tmp = "); Serial.print(Tmp/340.00+36.53); //eq. da temperatura (datasheet)
   Serial.print(" | GyX = "); Serial.print(GyX);
   Serial.print(" | GyY = "); Serial.print(GyY);
   Serial.print(" | GyZ = "); Serial.println(GyZ);
   delay(333);
 }
 
-
+"""
 #include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
@@ -84,3 +84,4 @@ void loop() {
 
   delay(10);
 }
+"""
